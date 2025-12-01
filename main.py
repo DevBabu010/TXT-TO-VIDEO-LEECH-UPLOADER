f3bf856de417dd71f1eba7a39bda0df38666edae
Shailendra Kumar Verma <neoshail010@gmail.com>
2025-12-01 11:06:37 +0530

    Update main.py3

--- main.py
+++ main.py
@@ -299,7 +299,7 @@ async def upload(bot: Client, m: Message):
     f'--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" '
     f'-f "{ytf}" "{url}" '
     f'-o "{name}.%(ext)s"'
-)
+
     
                         )
 
